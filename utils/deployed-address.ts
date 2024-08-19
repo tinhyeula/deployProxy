fc74288e97a9cb6b910fd8dc33d174613918bf4d
tinhyeula thu <ngongocvanajws24@gmail.com>
2024-08-19 07:27:50 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x36b509c521470d2f364191289fc341dDB240F2eb'
+export const ProxyAdmin = '0xfC301320C101b55D2376AFdf1f40940eeb21a1F3'
+export const SWTRImplementation = '0x180bb5B1b1A78267bBa44954661AA2C941A0BCFD'
     
